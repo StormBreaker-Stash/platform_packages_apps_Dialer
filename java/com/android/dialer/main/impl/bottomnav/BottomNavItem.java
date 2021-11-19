e5b5cfc66c2369cd53be66e47b2cfda3936bff91
Rahul <rahul731999@gmail.com>
2021-11-19 19:18:32 +0000

    Dialer: Implement Monet
    Change-Id: Ide1588f2ef31d7a717d334279ed74bdc326e6c67

--- java/com/android/dialer/main/impl/bottomnav/BottomNavItem.java
+++ java/com/android/dialer/main/impl/bottomnav/BottomNavItem.java
@@ -56,8 +56,8 @@ final class BottomNavItem extends LinearLayout {
     super.setSelected(selected);
     int colorId =
         selected
-            ? getContext().getResources().getColor(R.color.nav_item_selected)
-            : getContext().getResources().getColor(R.color.nav_item);
+            ? getResources().getColor(R.color.accent_color)
+            : getResources().getColor(R.color.dndn);
     image.setImageTintList(ColorStateList.valueOf(colorId));
     text.setTextColor(colorId);
   }
